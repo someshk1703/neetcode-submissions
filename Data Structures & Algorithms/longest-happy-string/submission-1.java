1d96c97eaa6a6f8ef32e16c53ac19f0a4a7b0a50
Somesh <143622516+someshk1703@users.noreply.github.com>
2026-07-04 22:36:29 +0530

    Add: longest-happy-string - submission-1

---
+++ Data Structures & Algorithms/longest-happy-string/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestDiverseString(int a, int b, int c) {
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((x,y) -> y[0]-x[0]);
+
+        if(a>0) maxHeap.offer(new int[]{a,'a'});
+        if(b>0) maxHeap.offer(new int[]{b,'b'});
+        if(c>0) maxHeap.offer(new int[]{c,'c'});
+
+        while(!maxHeap.isEmpty()){
+            int[] first = maxHeap.poll();
+            if(sb.length()>1 && sb.charAt(sb.length()-1)==first[1] && sb.charAt(sb.length()-2)==first[1]){
+                if(maxHeap.isEmpty()) break;
+                int[] second = maxHeap.poll();
+                sb.append((char) second[1]);
+                second[0]--;
+                if(second[0]>0) maxHeap.offer(second);
+                maxHeap.offer(first);
+            } else {
+                sb.append((char) first[1]);
+                first[0]--;
+                if(first[0]>0) maxHeap.offer(first);
+            }
+        }
+
+        return sb.toString();
+    }
+}
