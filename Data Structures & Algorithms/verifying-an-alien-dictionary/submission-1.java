00b77bbeaf34f8306b45b28ef1e85561f52a50ae
Somesh <143622516+someshk1703@users.noreply.github.com>
2026-07-15 21:46:37 +0530

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] orderidx = new int[26];
+        for(int i=0; i<order.length(); i++) orderidx[order.charAt(i)-'a']=i;
+
+        for(int i=0; i<words.length-1; i++){
+            String w1 = words[i], w2 = words[i+1];
+            int j = 0;
+
+            for(;j<w1.length();j++){
+                if(j==w2.length()) return false;
+                if(w1.charAt(j) != w2.charAt(j)){
+                    if(orderidx[w1.charAt(j)-'a']>orderidx[w2.charAt(j)-'a']) return false;
+                    break;
+                }
+            }
+        }
+        return true;
+
+    }
+}
