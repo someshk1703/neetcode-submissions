0f71b9f263cbd430449bfdda3bbdd1f86b5be5f8
Somesh <143622516+someshk1703@users.noreply.github.com>
2026-07-17 22:07:53 +0530

    Add: binary-tree-preorder-traversal - submission-7

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-7.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> res;
+    public List<Integer> preorderTraversal(TreeNode root) {
+        res = new ArrayList<>();
+
+        dfs(root);
+        return res;
+    }
+    public void dfs(TreeNode node){
+        if(node==null) return;
+
+        res.add(node.val);
+        dfs(node.left);
+        dfs(node.right);
+    }
+}
