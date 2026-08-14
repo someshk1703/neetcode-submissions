d80249ce80e525da459a23804f76d234830cd523
Somesh <143622516+someshk1703@users.noreply.github.com>
2026-08-14 20:27:13 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        boolean[] dp = new boolean[n];
+        dp[n-1] = true;
+
+        for(int i = n-2; i>=0; i--){
+            int end = Math.min(nums.length, i + nums[i] + 1);
+            for(int j=1; j<end; j++){
+                if(dp[j]){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
